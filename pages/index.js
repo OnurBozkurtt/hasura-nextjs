b6b5483fc379a5a8d91a9b84c35f4e3feb906bee
Onur Bozkurt <onurbozkurt166@gmail.com>
2022-10-17 17:47:40 +0300

    next.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(products) {
+  console.log(products);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +13,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul>
+          {/* {products.map((products) => <li key={products.id}>{products.name}</li>)} */}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -66,4 +36,28 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const options = {
+    method: 'post',
+    body: JSON.stringify({
+      query: `query fetchProducts{
+        products {
+          id
+          name
+        }
+      }`,
+      operationName: "fetchProducts"
+    })
+  }
+  const fetchResponse = await fetch('https://guiding-guppy-48.hasura.app/v1/graphql', options);
+  const responseJson = await fetchResponse.json();
+  console.log(responseJson);
+  const products = responseJson.data.products;
+  return {
+    props: {
+      products: products
+    }, // will be passed to the page component as props
+  }
 }
